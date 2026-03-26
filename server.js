cac9a93f09c4267125b1e2398a869156c21e9615
sablekalpesh07-ui <sablekalpesh07@gmail.com>
2026-03-26 22:01:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get("/", (req, res) => {
 
 /* ================= EMAIL ================= */
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
